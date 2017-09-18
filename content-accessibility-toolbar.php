83afc43b3fc85acabfdcf864fa7f17dfd40e3124
Ned Zimmerman <ned@bight.ca>
2017-09-18 14:53:31 -0300

    Footer tweaks.

--- content-accessibility-toolbar.php
+++ content-accessibility-toolbar.php
@@ -1,5 +1,11 @@
 <!-- a11y toolbar -->
 <div class="a11y-toolbar">
-  <ul><li><a href="#" role="button" class="a11y-toggle-fontsize toggle-fontsize" id="is_normal_fontsize" title="<?php _e( 'Toggle Font size', 'pressbooks-book' ); ?>"><span class="dashicons dashicons-visibility"></span></a></li></ul>
+  <ul>
+		<li>
+			<a href="#" role="button" class="a11y-toggle-fontsize toggle-fontsize" id="is_normal_fontsize" title="<?php _e( 'Toggle Font size', 'pressbooks-book' ); ?>">
+				<span class="dashicons dashicons-visibility"></span>
+			</a>
+		</li>
+	</ul>
 </div>
 <!-- // a11y toolbar -->
