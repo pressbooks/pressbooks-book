598898fdafb63ec6c7220ed9eff5bdfc470222ba
Ned Zimmerman <ned@bight.ca>
2018-01-11 13:51:15 -0400

    Fix build.

--- packages/aetna/webpack.mix.js
+++ packages/aetna/webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require("laravel-mix");
  |
  */
 
-mix.sass("src/components/button/style.scss", "src/components/button/");
+mix.sass("assets/styles/aetna.scss", "public/styles");
 
 // Full API
 // mix.js(src, output);
