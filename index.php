2129ea8a1051780c50eeb9f7aaa82ebd92770311
Ned Zimmerman <ned@bight.ca>
2018-02-05 12:52:29 -0400

    Fix TOC.

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 <?php if ( pb_is_public() ) : ?>
-	<?php get_template_part( 'partials/content-toc.php' ); ?>
+	<section class="block block-toc">
+		<h2 class="block__title block-toc__title"><?php _e( 'Contents', 'pressbooks-book' ); ?></h2>
+		<?php include( locate_template( 'partials/content-toc.php' ) ); ?>
+	</section>
 <?php else : ?>
 	<?php pb_private(); ?>
 <?php endif; ?>
