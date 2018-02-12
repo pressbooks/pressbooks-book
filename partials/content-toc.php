9982d5d55f9ab584f7e86058e21da2e2921a0e71
Ned Zimmerman <ned@bight.ca>
2018-02-12 15:31:43 -0500

    Fix empty part display in contents (closes #112)

--- partials/content-toc.php
+++ partials/content-toc.php
@@ -14,34 +14,27 @@ if ( $output !== false ) {
 		</li>
 		<?php
 		$n = 0;
-		foreach ( $book_structure['part'] as $key => $part ) : ?>
-			<li class="js-toc-part<?php if ( ! empty( $part['chapters'] ) ) { ?> dropdown<?php } ?>">
-				<?php if ( count( $book_structure['part'] ) > 1  && get_post_meta( $part['ID'], 'pb_part_invisible', true ) !== 'on' ) {
+		foreach ( $book_structure['part'] as $key => $part ) :
+			if ( ! empty( $part['chapters'] ) || $part['has_post_content'] ) { ?>
+			<li class="toc__part<?php if ( ! empty( $part['chapters'] ) ) { ?> dropdown<?php } ?>"><?php
+			if ( count( $book_structure['part'] ) > 1 && get_post_meta( $part['ID'], 'pb_part_invisible', true ) !== 'on' ) {
 					$n++; ?>
-					<h3 class="toc__part__title js-toc-part-toggle">
+					<h3 class="toc__part__title">
 						<span class="inner-content"><?php
-						if ( $part['has_post_content'] ) {
-							?>
-							<a href="<?php echo get_permalink( $part['ID'] ); ?>"><?php
-						} ?>
-							<?php if ( $part_numbers ) : ?><span><?php echo \Pressbooks\L10n\romanize( $n ); ?>. </span><?php endif; ?>
-
-							<?php echo $part['post_title'];
-
-							if ( $part['has_post_content'] ) {
-								?></a><?php
-							}
+						if ( $part['has_post_content'] ) { ?><a href="<?php echo get_permalink( $part['ID'] ); ?>"><?php }
+						if ( $part_numbers ) { ?><span><?php echo \Pressbooks\L10n\romanize( $n ); ?>. </span><?php }
+						echo $part['post_title'];
+						if ( $part['has_post_content'] ) { ?></a><?php }
 						?></span>
-					</h3>
-				<?php } ?>
+					</h3><?php }
+			if ( ! empty( $part['chapters'] ) ) { ?>
 				<div class="inner-content">
 					<ol class="toc__chapters">
 						<?php \Pressbooks\Book\Helpers\toc_sections( $part['chapters'], 'chapter', $can_read, $can_read_private, $permissive_private_content, $should_parse_subsections ); ?>
 					</ol>
 				</div>
-			</li>
-		<?php if ( ! empty( $part['chapters'] ) || $part['has_post_content'] ) {
-} endforeach; ?>
+				<?php } ?></li><?php }
+		endforeach; ?>
 		<li class="dropdown">
 			<h3 class="toc__back-matter__title"><?php _e( 'Back Matter', 'pressbooks' ); ?></h3>
 			<ol class="toc__back-matter-list">
