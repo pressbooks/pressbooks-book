1a2cd8a4c7852bcafbacc6a79cf86f59685061c6
Dac Chartrand <dac.chartrand@gmail.com>
2018-03-19 11:52:06 -0400

    Fix Travis CI

--- 404.php
+++ 404.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
 	<h1><?php esc_html_e( 'Oops! That chapter can&rsquo;t be found.', 'pressbooks-book' ); ?></h1>
 	<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'pressbooks-book' ); ?></p>
-<?php get_footer(); ?>
+<?php get_footer();
