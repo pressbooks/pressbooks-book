66894c8ececd52fbd1d77a712474ce653da82264
Ned Zimmerman <ned@bight.ca>
2018-03-19 12:01:19 -0400

    Styling updates.

--- 404.php
+++ 404.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
-	<h1><?php esc_html_e( 'Oops! That chapter can&rsquo;t be found.', 'pressbooks-book' ); ?></h1>
-	<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'pressbooks-book' ); ?></p>
+	<h1><?php esc_html_e( 'Oops! That content can&rsquo;t be found.', 'pressbooks-book' ); ?></h1>
+	<p><?php esc_html_e( 'It looks like nothing was found at this location. Try a search?', 'pressbooks-book' ); ?></p>
+	<?php get_search_form(); ?>
 <?php get_footer();
