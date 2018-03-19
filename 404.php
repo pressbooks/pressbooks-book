8fb71799b9bb69b6d67a3bcac438ed0854525c3f
Dac Chartrand <dac.chartrand@gmail.com>
2018-03-19 11:38:21 -0400

    404 page (issue #145)

--- 404.php
+++ 404.php
@@ -1,1 +1,4 @@
-<?php wp_redirect( esc_url( network_home_url() ), 301 );
+<?php get_header(); ?>
+	<h1><?php esc_html_e( 'Oops! That chapter can&rsquo;t be found.', 'pressbooks-book' ); ?></h1>
+	<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'pressbooks-book' ); ?></p>
+<?php get_footer(); ?>
