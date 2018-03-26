6095306ad2ee8c41588a0bf21c456f589c46963f
Ned Zimmerman <ned@bight.ca>
2018-03-26 16:33:22 -0300

    Add show/hide buttons to cover TOC (fix #146).

--- assets/src/scripts/routes/home.js
+++ assets/src/scripts/routes/home.js
@@ -1,28 +1,42 @@
 export default {
 	init() {
 		jQuery( $ => {
-			// $( document.body ).on( 'click', function ( e ) {
-			// 	let $target = $( e.target );
-			// 	if ( $target.is( '[data-toggle=dropdown]' ) ) {
-			// 		return;
-			// 	}
-
-			// 	$( '.dropdown-menu.show' ).each( function () {
-			// 		let $dropdown = $( this );
-			// 		$dropdown.removeClass( 'show' );
-			// 	} );
-			// } );
-
-			// $( document.body ).on( 'click', '[data-toggle=dropdown]', function ( e ) {
-			// 	let $target = $( this );
-			// 	let $dropdown = $target.parent( '.dropdown' ).find( '.dropdown-menu' );
-			// 	$dropdown.toggleClass( 'show' );
-			// } );
-
 			$( document.body ).on( 'click', '.js-toggle-block', function ( e ) {
 				let $target = $( this );
 				$target.parents( '.js-block' ).toggleClass( 'block-toggle--visible' );
 				$target.toggleClass( '--visible' );
+			} );
+
+			$( document.body ).on( 'click', '.toc__toggle #show', function ( e ) {
+				let $target = $( this );
+				let $dropdowns = $( '.toc__list .dropdown' );
+				$dropdowns.each( function ( el ) {
+					$( this )
+						.find( 'button' )
+						.attr( 'aria-expanded', 'true' );
+					$( this )
+						.find(
+							'h3 + .inner-content, .toc__front-matter-list, .toc__back-matter-list'
+						)
+						.removeAttr( 'hidden' );
+				} );
+				$target.parents( '.toc__toggle' ).attr( 'aria-expanded', 'true' );
+			} );
+
+			$( document.body ).on( 'click', '.toc__toggle #hide', function ( e ) {
+				let $target = $( this );
+				let $dropdowns = $( '.toc__list .dropdown' );
+				$dropdowns.each( function ( el ) {
+					$( this )
+						.find( 'button' )
+						.attr( 'aria-expanded', 'false' );
+					$( this )
+						.find(
+							'h3 + .inner-content, .toc__front-matter-list, .toc__back-matter-list'
+						)
+						.attr( 'hidden', 'true' );
+				} );
+				$target.parents( '.toc__toggle' ).attr( 'aria-expanded', 'false' );
 			} );
 		} );
 	},
--- partials/content-cover-toc.php
+++ partials/content-cover-toc.php
@@ -1,8 +1,12 @@
 <section class="block block-toc">
 	<?php if ( pb_get_first_post_id() ) : ?>
 		<h2 class="block__title block-toc__title"><?php _e( 'Contents', 'pressbooks-book' ); ?></h2><?php
-		include( locate_template( 'partials/content-toc.php' ) );
-	endif;
+		include( locate_template( 'partials/content-toc.php' ) ); ?>
+		<div class="toc__toggle" aria-expanded="false">
+			<button id="show" class="button"><?php _e( 'Show All Contents' ); ?></button>
+			<button id="hide" class="button"><?php _e( 'Hide All Contents' ); ?></button>
+		</div>
+	<?php endif;
 	/**
 	 * Append content to cover table of contents block.
 	 *
