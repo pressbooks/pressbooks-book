49406c86c751bbf58ebd88784602958933b32f7e
Dac Chartrand <dac.chartrand@gmail.com>
2018-03-27 18:13:02 -0400

    Fix TOC caching (#162)
    When using redis (or memcached) transients are not in the database so querying for them in the DB was returning nothing.
    
    The fix is to delete every combination of the transient for good measure.

--- inc/actions/namespace.php
+++ inc/actions/namespace.php
@@ -4,12 +4,19 @@ namespace Pressbooks\Book\Actions;
 
 /**
  * Delete the cached Table of Contents.
+ *
+ * @see partials/content-toc.php
  */
 function delete_cached_contents() {
-	global $wpdb;
-	$results = $wpdb->get_results( "SELECT option_name FROM {$wpdb->options} WHERE option_name LIKE ('_transient_pb_book_contents%')" );
-	foreach ( $results as $val ) {
-		$transient = str_replace( '_transient_', '', $val->option_name );
+	$bits = 3;
+	$max = ( 1 << $bits );
+	$transients = [];
+	for ( $i = 0; $i < $max; $i++ ) {
+		$t = str_pad( decbin( $i ), $bits, '0', STR_PAD_LEFT );
+		$t = str_replace( [ 1, 0 ], [ 'x', 'o' ], $t );
+		$transients[] = "pb_book_contents_{$t}";
+	}
+	foreach ( $transients as $transient ) {
 		delete_transient( $transient );
 	}
 }
