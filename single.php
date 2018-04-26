d77ab52101880d414941fcfdd7ac37f7a4951f3f
Ned Zimmerman <ned@bight.ca>
2018-04-26 16:12:55 -0300

    Make it happen.

--- single.php
+++ single.php
@@ -24,38 +24,8 @@
 
 					<div class="block block-reading-meta">
 						<div class="block-reading-meta__inner">
+							<?php include( locate_template( 'partials/content-difftool.php' ) ); ?>
 
-							<?php $meta = new \Pressbooks\Metadata();
-							$pb_book_is_based_on = get_post_meta( $meta->getMetaPost()->ID, 'pb_is_based_on', true );
-							$pb_section_is_based_on = get_post_meta( $post->ID, 'pb_is_based_on', true );
-							if ( $pb_book_is_based_on ) {
-								$source_url = \Pressbooks\Book\Helpers\get_source_book_url( $pb_book_is_based_on );
-								$source_meta = \Pressbooks\Book\Helpers\get_source_book_meta( $source_url );
-								$source_toc = \Pressbooks\Book\Helpers\get_source_book_toc( $source_url );
-								$original = \Pressbooks\Book\Helpers\get_original_section( $pb_section_is_based_on, $source_toc );
-								if ( $original ) {
-									$source_endpoint = implode( '/', [
-										$source_url,
-										'wp-json/pressbooks/v2',
-										( in_array( $post->post_type, [ 'part', 'chapter' ], true ) ) ? $post->post_type . 's' : $post->post_type,
-										$original['id'],
-									] );
-									?>
-									<div class="block-reading-meta__compare">
-<p><?php printf(
-	__( 'This chapter is adapted from %1$s in %2$s by %3$s.', 'pressbooks-book' ),
-	sprintf( '<a href="%1$s">%2$s</a>', $original['link'], $original['title'] ),
-	sprintf( '<a href="%1$s">%2$s</a>', $source_url, $source_meta['name'] ),
-\Pressbooks\Book\Helpers\get_book_authors( $source_meta ) );
-?>
-									</p>
-									<button id="compare"><?php _e( 'Compare with Original', 'pressbooks' ); ?></button>
-									<pre
-										id="diff"
-										data-source-endpoint="<?php echo $source_endpoint ?>" hidden><?php echo get_post_field( 'post_content', $post, 'raw' ); ?></pre>
-								</div>
-								<?php }
-							} ?>
 							<div class="block-reading-meta__subsection">
 								<h2 class="section__subtitle block-reading-meta__subtitle"><?php _e( 'License','pressbooks-book' ); ?></h2>
 								<?php if ( pb_is_public() ) { ?>
