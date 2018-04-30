a3f67a81f810adec861cc511ee81f66399261899
Ned Zimmerman <ned@bight.ca>
2018-04-30 11:43:55 -0300

    Label search field to clarify scope (fix #171).

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo _ex( 'Search for:', 'label', 'pressbooks-book' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'pressbooks-book' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<span class="screen-reader-text"><?php echo _ex( 'Search in book:', 'label', 'pressbooks-book' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search in book &hellip;', 'placeholder', 'pressbooks-book' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
 	<button type="submit" class="search-submit">
 		<svg class="icon--svg"><use xlink:href="#search" /></svg>
