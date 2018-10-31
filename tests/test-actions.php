96b420e356e7a57d19e2b4dec68d48886349bda1
Ned Zimmerman <ned@bight.ca>
2018-10-31 15:31:57 -0300

    Test theme setup

--- tests/test-actions.php
+++ tests/test-actions.php
@@ -5,8 +5,8 @@
  * @package Pressbooks_Book
  */
 
-use function \Pressbooks\Book\Actions\add_lightbox_setting;
 use function \Pressbooks\Book\Actions\render_lightbox_setting_field;
+use function \Pressbooks\Book\Actions\theme_setup;
 
 /**
  * Helpers test case.
@@ -17,5 +17,10 @@ class ActionsTest extends WP_UnitTestCase {
 		render_lightbox_setting_field( [ 'Show linked images in a lightbox' ] );
 		$buffer = ob_get_clean();
 		$this->assertEquals( '<input id="enable_lightbox" name="pressbooks_theme_options_web[enable_lightbox]" type="checkbox" value="1" /><label for="enable_lightbox">Show linked images in a lightbox</label>', $buffer );
+	}
+
+	function test_theme_setup() {
+		theme_setup();
+		$this->assertEquals( [ 'caption' ], get_theme_support( 'html5' ) )
 	}
 }
