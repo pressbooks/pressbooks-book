77110c2ab95241f93efee5d6a1a3875daa37b022
Ned Zimmerman <ned@bight.ca>
2018-11-07 11:41:40 -0400

    Increase code coverage

--- tests/test-helpers.php
+++ tests/test-helpers.php
@@ -5,6 +5,8 @@
  * @package Pressbooks_Book
  */
 
+use function \Pressbooks\Book\Helpers\count_authors;
+use function \Pressbooks\Book\Helpers\display_menu;
 use function \Pressbooks\Book\Helpers\get_book_authors;
 use function \Pressbooks\Book\Helpers\get_metakeys;
 use function \Pressbooks\Book\Helpers\get_name_for_filetype;
@@ -12,23 +14,22 @@ use function \Pressbooks\Book\Helpers\get_source_book;
 use function \Pressbooks\Book\Helpers\get_source_book_meta;
 use function \Pressbooks\Book\Helpers\get_source_book_toc;
 use function \Pressbooks\Book\Helpers\get_source_book_url;
+use function \Pressbooks\Book\Helpers\is_book_public;
 use function \Pressbooks\Book\Helpers\license_to_icons;
 use function \Pressbooks\Book\Helpers\license_to_text;
 use function \Pressbooks\Book\Helpers\share_icons;
+use function \Pressbooks\Book\Helpers\social_media_enabled;
 
 /**
  * Helpers test case.
  */
 class HelpersTest extends WP_UnitTestCase {
-	function test_get_metakeys() {
-		$output = get_metakeys();
-		$this->assertInternalType( 'array', $output );
-	}
 
 	function test_get_name_for_filetype() {
 		$output = get_name_for_filetype( 'pdf' );
 		$this->assertEquals( 'Digital PDF', $output );
 	}
+
 	function test_license_to_icons() {
 		$output = license_to_icons( 'cc-by' );
 		$this->assertEquals( '<svg class="icon" style="fill: currentColor"><use xlink:href="#cc" /></svg><svg class="icon" style="fill: currentColor"><use xlink:href="#cc-by" /></svg>', $output );
@@ -41,6 +42,7 @@ class HelpersTest extends WP_UnitTestCase {
 		$output = license_to_icons( 'foo' );
 		$this->assertEquals( '', $output );
 	}
+
 	function test_license_to_text() {
 		$output = license_to_text( 'cc-by' );
 		$this->assertEquals( 'Creative Commons Attribution', $output );
@@ -49,17 +51,27 @@ class HelpersTest extends WP_UnitTestCase {
 		$output = license_to_text( 'foo' );
 		$this->assertEquals( 'All Rights Reserved', $output );
 	}
+
 	function test_share_icons() {
 		$this->assertStringStartsWith( '<a class="sharer" data-sharer="twitter" data-title="Check out this great book on Pressbooks."', share_icons() );
 	}
+
+	function test_display_menu() {
+		$result = display_menu();
+		$this->assertInternalType( 'string', $result );
+		$this->assertContains( sprintf( '<li><a href="%1$s">Home</a></li>', get_home_url() ), $result );
+	}
+
 	function test_get_source_book() {
 		$output = get_source_book( 'https://book.pressbooks.com' );
 		$this->assertEquals( 'https://book.pressbooks.com', $output );
 	}
+
 	function test_get_source_book_url() {
 		$output = get_source_book_url( 'https://book.pressbooks.com' );
 		$this->assertEquals( 'https://book.pressbooks.com', $output );
 	}
+
 	function test_get_source_book_meta() {
 		$output = get_source_book_toc( 'garbage/' );
 		$this->assertFalse( $output );
@@ -67,6 +79,15 @@ class HelpersTest extends WP_UnitTestCase {
 		$output = get_source_book_meta( 'https://book.pressbooks.com' );
 		$this->assertArrayHasKey( 'name', $output );
 		$this->assertEquals( "Book: A Futurist's Manifesto", $output['name'] );
+	}
+
+	function test_get_source_book_toc() {
+		$results = get_source_book_toc( 'garbage/' );
+		$this->assertFalse( $results );
+
+		$results = get_source_book_toc( 'https://book.pressbooks.com/' );
+		$this->assertTrue( is_array( $results ) );
+		$this->assertNotEmpty( $results );
 	}
 
 	function test_get_book_authors() {
@@ -111,12 +132,29 @@ class HelpersTest extends WP_UnitTestCase {
 		$this->assertEquals( 'First, Second, and Third', $output );
 	}
 
-	function test_get_source_book_toc() {
-		$results = get_source_book_toc( 'garbage/' );
-		$this->assertFalse( $results );
+	function test_get_metakeys() {
+		$output = get_metakeys();
+		$this->assertInternalType( 'array', $output );
+	}
 
-		$results = get_source_book_toc( 'https://book.pressbooks.com/' );
-		$this->assertTrue( is_array( $results ) );
-		$this->assertNotEmpty( $results );
+	function test_social_media_enabled() {
+		$result = social_media_enabled();
+		$this->assertTrue( $result );
+	}
+
+	function test_is_book_public() {
+		$result = is_book_public();
+		$this->assertTrue( $result );
+	}
+
+	function test_count_authors() {
+		$result = count_authors( 'Author One, Author Two, and Author Three' );
+		$this->assertEquals( 3, $result );
+		$result = count_authors( 'Author One and Author Two' );
+		$this->assertEquals( 2, $result );
+		$result = count_authors( 'Author One' );
+		$this->assertEquals( 1, $result );
+		$result = count_authors( '' );
+		$this->assertEquals( 0, $result );
 	}
 }
