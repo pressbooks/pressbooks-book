eaff4fdc73e06e83e3ffff1c427f79afe1f361f3
Dac Chartrand <dac.chartrand@gmail.com>
2019-03-20 09:45:36 -0400

    Fix Windows compatibility when building

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 let mix = require( 'laravel-mix' );
+let path = require( 'path' );
+let normalizeNewline = require( 'normalize-newline' );
+let fs = require( 'fs' );
 
 /*
  |--------------------------------------------------------------------------
@@ -12,6 +15,20 @@ let mix = require( 'laravel-mix' );
  */
 
 const dist = 'dist';
+
+// Normalize Newlines
+const normalizeNewlines = ( dir ) => {
+	fs.readdirSync( dir ).forEach( function( file ) {
+		file = path.join( dir, file );
+		fs.readFile( file, 'utf8', function( err, buffer ) {
+			if ( err ) return console.log( err );
+			buffer = normalizeNewline( buffer );
+			fs.writeFile( file, buffer, 'utf8', function( err ) {
+				if ( err ) return console.log( err );
+			} );
+		} );
+	} );
+};
 
 mix
 	.setPublicPath( 'dist' )
@@ -31,7 +48,11 @@ mix
 	.copy( 'node_modules/lity/dist/lity.css', 'dist/styles/lity.css' )
 	.copyDirectory( 'assets/src/images', 'dist/images' )
 	.version()
-	.options( { processCssUrls: false } );
+	.options( { processCssUrls: false } )
+	.then( () => {
+		normalizeNewlines( 'dist/scripts/' );
+		normalizeNewlines( 'dist/styles/' );
+	} );
 
 // BrowserSync
 mix.browserSync( {
