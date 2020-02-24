1acddeddfb1b5d96c00962381a564dfa58405007
Carlos Gutiérrez <carlos@pressbooks.com>
2020-02-24 10:31:34 -0600

    Remove deprecated feature 'increase font size' (#673)

--- partials/content-accessibility-toolbar.php
+++
@@ -1,7 +0,0 @@
-<!-- a11y toolbar -->
-<div class="a11y-toolbar">
-	<button class="a11y-fontsize" aria-pressed="false">
-		<?php _e( 'Increase Font Size', 'pressbooks-book' ); ?>
-	</button>
-</div>
-<!-- // a11y toolbar -->
