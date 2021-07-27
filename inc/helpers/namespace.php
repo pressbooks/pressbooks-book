f93d8b3b457e17e7d93ae3678a32f3bdda0c80db
Ricardo Aragon <richard015ar@gmail.com>
2021-07-27 10:42:28 -0400

    Add classes for nav primary menu items and footer items

--- inc/helpers/namespace.php
+++ inc/helpers/namespace.php
@@ -187,48 +187,73 @@ function share_icons() {
  * @return string The primary menu contents.
  */
 function display_menu() {
+	$id_items = [
+		'prefix' => 'nav--primary-item',
+		'Home' => 'home',
+		'Read' => 'read',
+		'Buy' => 'buy',
+		'SignIn' => 'sign-in',
+		'Admin' => 'admin',
+		'SignOut' => 'sign-out',
+		'Search' => 'search',
+	];
+
 	$items = sprintf(
-		'<li><a href="%1$s">%2$s</a></li>',
+		'<li class="%3$s %3$s-%4$s"><a href="%1$s">%2$s</a></li>',
 		( is_front_page() ) ? '#main' : get_home_url(),
-		__( 'Home', 'pressbooks-book' )
+		__( 'Home', 'pressbooks-book' ),
+		$id_items['prefix'],
+		$id_items['Home']
 	);
 	if ( pb_get_first_post_id() ) {
 		$items .= sprintf(
-			'<li><a href="%1$s">%2$s</a></li>',
+			'<li class="%3$s %3$s-%4$s"><a href="%1$s">%2$s</a></li>',
 			pb_get_first(),
-			__( 'Read', 'pressbooks-book' )
+			__( 'Read', 'pressbooks-book' ),
+			$id_items['prefix'],
+			$id_items['Read']
 		);
 	}
 	if ( array_filter( get_option( 'pressbooks_ecommerce_links', [] ) ) ) {
 		$items .= sprintf(
-			'<li><a href="%1$s">%2$s</a></li>',
+			'<li class="%3$s %3$s-%4$s"><a href="%1$s">%2$s</a></li>',
 			( get_page_link() === home_url( '/buy/' ) ) ? '#main' : home_url( '/buy/' ),
-			__( 'Buy', 'pressbooks-book' )
+			__( 'Buy', 'pressbooks-book' ),
+			$id_items['prefix'],
+			$id_items['Buy']
 		);
 	}
 	if ( ! is_user_logged_in() ) {
 		$items .= sprintf(
-			'<li><a href="%1$s">%2$s</a></li>',
+			'<li class="%3$s %3$s-%4$s"><a href="%1$s">%2$s</a></li>',
 			wp_login_url( get_permalink() ),
-			__( 'Sign in', 'pressbooks-book' )
+			__( 'Sign in', 'pressbooks-book' ),
+			$id_items['prefix'],
+			$id_items['SignIn']
 		);
 	} else {
 		if ( is_super_admin() || is_user_member_of_blog() ) {
 			$items .= sprintf(
-				'<li><a href="%1$s">%2$s</a></li>',
+				'<li class="%3$s %3$s-%4$s"><a href="%1$s">%2$s</a></li>',
 				admin_url(),
-				__( 'Admin', 'pressbooks-book' )
+				__( 'Admin', 'pressbooks-book' ),
+				$id_items['prefix'],
+				$id_items['Admin']
 			);
 		}
 		$items .= sprintf(
-			'<li><a href="%1$s">%2$s</a></li>',
+			'<li class="%3$s %3$s-%4$s"><a href="%1$s">%2$s</a></li>',
 			wp_logout_url( get_permalink() ),
-			__( 'Sign out', 'pressbooks-book' )
+			__( 'Sign out', 'pressbooks-book' ),
+			$id_items['prefix'],
+			$id_items['SignOut']
 		);
 	}
 	$items .= sprintf(
-		'<li class="header__search js-search"><div class="header__search__form">%s</div></li>',
-		get_search_form( false )
+		'<li class="header__search js-search %2$s %2$s-%3$s"><div class="header__search__form">%1$s</div></li>',
+		get_search_form( false ),
+		$id_items['prefix'],
+		$id_items['Search']
 	);
 
 	return $items;
