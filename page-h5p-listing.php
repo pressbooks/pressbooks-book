c94e5041dddc167f7a68d8d091e435eef8d87bfd
SteelWagstaff <steel@pressbooks.com>
2021-07-28 15:16:04 -0700

    Modifications to DOM structure and styling

--- page-h5p-listing.php
+++ page-h5p-listing.php
@@ -5,26 +5,20 @@ if ( \PressbooksBook\Helpers\is_book_public() ) :
 	$data = \PressbooksBook\Helpers\get_h5p_activities();
 	?>
 	<div id="post-<?php the_ID(); ?>" <?php post_class( 'h5p-listing-page' ); ?>>
-			<h2 class="page-title"><?php echo __( 'H5P activities list', 'pressbooks-book' ); ?></h2>
-
-			<h3 class="page-subtitle">
+			<h1 class="page-title"><?php echo __( 'H5P activities list', 'pressbooks-book' ); ?></h1>
+			<p class="h5p-count">
 				<?php echo __( 'This book includes ', 'pressbooks-book' ); ?>
-				<span><?php echo $data['total'] . ' '; ?></span>
-			<?php echo __( 'h5p activities', 'pressbooks-book' ); ?></h3>
-
-			<div class="float-right">
-					<?php echo sprintf( '<button type="button" id="h5p-show-hide" class="btn btn-secondary btn-sm" show-all-text="%s" hide-all-text="%s" ></button>', __( 'Expand all', 'pressbooks-book' ), __( 'Hide all', 'pressbooks-book' ) ); ?>
-			</div>
-
-			<br /><br />
-
-			<table class="table table-borderless">
+				<strong><?php echo $data['total'] . ' '; ?></strong>
+			<?php echo __( 'H5P activities. Only those which have been inserted into book content will be included if the book is cloned.', 'pressbooks-book' ); ?>
+			<?php echo sprintf( '<button type="button" id="h5p-show-hide" class="btn btn-secondary btn-sm" show-all-text="%s" hide-all-text="%s" ></button>', __( 'Expand all', 'pressbooks-book' ), __( 'Hide all', 'pressbooks-book' ) ); ?>
+			</p>
+			<table class="table table-borderless" id="h5p-listing-table">
 				<thead>
 				<tr>
 					<th><?php echo __( 'ID', 'pressbooks-book' ); ?></th>
 					<th><?php echo __( 'Title', 'pressbooks-book' ); ?></th>
 					<th><?php echo __( 'Activity type', 'pressbooks-book' ); ?></th>
-					<th><?php echo __( 'View', 'pressbooks-book' ); ?></th>
+					<th><?php echo __( 'Show/Hide', 'pressbooks-book' ); ?></th>
 				</tr>
 				</thead>
 				<tbody>
@@ -36,14 +30,13 @@ if ( \PressbooksBook\Helpers\is_book_public() ) :
 					echo sprintf( '<td>%s</td>', $activity['ID'] );
 					echo sprintf( '<td>%s</td>', $activity['title'] );
 					echo sprintf( '<td>%s</td>', $activity['activity_type'] );
-					echo sprintf( '<td><button h5p-id="%s" type="button" show-activity-text="%s" hide-activity-text="%s" class="btn btn-secondary btn-sm h5p-row-item"></button></td>', $activity['ID'], __( 'View activity', 'pressbooks-book' ), __( 'Hide activity', 'pressbooks-book' ) );
+					echo sprintf( '<td><button h5p-id="%s" type="button" show-activity-text="%s" hide-activity-text="%s" class="btn btn-secondary btn-sm h5p-row-item"></button></td>', $activity['ID'], __( 'Show activity', 'pressbooks-book' ), __( 'Hide activity', 'pressbooks-book' ) );
 					echo '</tr>';
 					echo sprintf( '<tr class="h5p-activity-container"><td colspan="4">%s</td></tr>', do_shortcode( $short_code ) );
 				}
 				?>
 				</tbody>
 			</table>
-
 		<div class="h5p-pagination">
 			<?php echo $data['pagination']; ?>
 		</div>
