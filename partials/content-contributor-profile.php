6daef2596e462c4b17bdc166aa7099ca3131177e
SteelWagstaff <steel@pressbooks.com>
2021-09-16 11:02:11 -0700

    Add conditional check before displaying links div, SCSS fixes

--- partials/content-contributor-profile.php
+++ partials/content-contributor-profile.php
@@ -1,6 +1,6 @@
 <div class="contributor__name__and__links">
 	<?php if ( $contributor['contributor_picture'] ) : ?>
-		<img class="contributor__profile__picture" alt="" title="" src="<?php echo $contributor['contributor_picture'] ?>"/>
+		<img class="contributor__profile__picture" alt="" role="presentation" src="<?php echo $contributor['contributor_picture'] ?>"/>
 		<?php endif; ?>
 	<p class="contributor__name"><span class="screen-reader-text">name: </span><?php echo $contributor['name'] ?></p>
 	<?php if ( $contributor['contributor_institution'] ) : ?>
@@ -9,6 +9,7 @@
 	<?php if ( $contributor['contributor_user_url'] ) : ?>
 		<p class="contributor__website"><span class="screen-reader-text">website: </span><a href="<?php echo $contributor['contributor_user_url'] ?>" target="_blank"><?php echo $contributor['contributor_user_url'] ?></a></p>
 	<?php endif; ?>
+	<?php if ( $contributor['contributor_twitter'] || $contributor['contributor_linkedin'] || $contributor['contributor_github'] ) : ?>
 	<div class="contributor__links">
 		<?php if ( $contributor['contributor_twitter'] ) : ?>
 			<a class="contributor__twitter" href="<?php echo $contributor['contributor_twitter'] ?>" target="_blank"><svg role="img" aria-labelledby="twitter-logo" class="contributor icon-svg"><title id="twitter-title-logo">Twitter logo</title><use href="#twitter-icon"/></svg></a>
@@ -20,5 +21,6 @@
 			<a class="contributor__github" href="<?php echo $contributor['contributor_github'] ?>" target="_blank"><svg role="img" aria-labelledby="github-logo" class="contributor icon-svg"><title id="github-title-logo">GitHub logo</title><use href="#github-icon"/></svg></a>
 			<?php endif; ?>
 	</div>
+	<?php endif; ?>
 </div>
 <div class="contributor__bio"><?php echo wp_kses( $contributor['contributor_description'], true ) ?></div>
