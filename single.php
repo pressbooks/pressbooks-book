f33f9427ac77d1d98bc2c78381d590221c44906e
SteelWagstaff <steel@pressbooks.com>
2021-10-25 19:36:42 -0700

    Fix standards

--- single.php
+++ single.php
@@ -33,7 +33,8 @@
 
 							<div class="block-reading-meta__subsection">
 								<h2 class="section__subtitle block-reading-meta__subtitle"><?php _e( 'License', 'pressbooks-book' ); ?></h2>
-								<?php if ( \PressbooksBook\Helpers\is_book_public() ) {
+								<?php
+								if ( \PressbooksBook\Helpers\is_book_public() ) {
 									echo \PressbooksBook\Helpers\copyright_license( false );
 								}
 								$pb_section_doi = get_post_meta( $post->ID, 'pb_section_doi', true );
@@ -42,13 +43,13 @@
 									<h2 class="section__subtitle block-reading-meta__subtitle"><?php _e( 'Digital Object Identifier (DOI)', 'pressbooks-book' ); ?></h2>
 									<p>
 									<?php
-										/**
-										 * Filter the DOI resolver service URL (default: https://dx.doi.org).
-										 *
-										 * @since Pressbooks @ 5.6.0
-										 */
-										$doi_resolver = apply_filters( 'pb_doi_resolver', 'https://dx.doi.org' );
-										printf( '<a itemprop="sameAs" href="%1$s">%1$s</a>', trailingslashit( $doi_resolver ) . $pb_section_doi );
+									/**
+									 * Filter the DOI resolver service URL (default: https://dx.doi.org).
+									 *
+									 * @since Pressbooks @ 5.6.0
+									 */
+									$doi_resolver = apply_filters( 'pb_doi_resolver', 'https://dx.doi.org' );
+									printf( '<a itemprop="sameAs" href="%1$s">%1$s</a>', trailingslashit( $doi_resolver ) . $pb_section_doi );
 									?>
 									</p>
 								<?php } ?>
