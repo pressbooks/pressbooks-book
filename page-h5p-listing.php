fcb5b2f341f6e652d20d5a32145139f90ce941ea
Ricardo Aragon <richard015ar@gmail.com>
2021-12-08 08:56:29 -0500

    Improve list page for 1 single h5p activity

--- page-h5p-listing.php
+++ page-h5p-listing.php
@@ -6,45 +6,51 @@ if ( \PressbooksBook\Helpers\is_book_public() ) :
 	$activities = count( $data['activities'] );
 	?>
 	<div id="post-<?php the_ID(); ?>" <?php post_class( 'h5p-listing-page' ); ?>>
-			<h1 class="page-title"><?php echo __( 'H5P activities list', 'pressbooks-book' ); ?></h1>
+		<h1 class="page-title">
+			<?php
+			echo isset( $_GET['h5p_id'] ) ?
+				$data['activities'][0]['title'] :
+				__( 'H5P activities list', 'pressbooks-book' );
+			?>
+		</h1>
+		<?php if ( ! isset( $_GET['h5p_id'] ) ) : ?>
 			<p class="h5p-count">
 				<?php echo __( 'This book includes ', 'pressbooks-book' ); ?>
 				<strong><?php echo $data['total'] . ' '; ?></strong>
-			<?php echo __( 'H5P activities. Only those which have been inserted into book content will be included if the book is cloned.', 'pressbooks-book' ); ?>
+				<?php echo __( 'H5P activities. Only those which have been inserted into book content will be included if the book is cloned.', 'pressbooks-book' ); ?>
+				<?php echo sprintf( '<button type="button" id="h5p-show-hide" class="btn btn-secondary btn-sm" show-all-text="%s" hide-all-text="%s" aria-label="%s"></button>', __( 'Expand all', 'pressbooks-book' ), __( 'Hide all', 'pressbooks-book' ), __( 'Expand all activities', 'pressbooks-book' ) ); ?>
+			</p>
+		<?php endif; ?>
+		<table class="table table-borderless" id="h5p-listing-table">
+			<thead>
+			<tr>
+				<th><?php echo __( 'ID', 'pressbooks-book' ); ?></th>
+				<th><?php echo __( 'Title', 'pressbooks-book' ); ?></th>
+				<th><?php echo __( 'Activity type', 'pressbooks-book' ); ?></th>
+				<?php if ( ! isset( $_GET['h5p_id'] ) ) : ?>
+					<th><?php echo __( 'Show/Hide', 'pressbooks-book' ); ?></th>
+				<?php endif; ?>
+			</tr>
+			</thead>
+			<tbody>
 			<?php
-			if ( $activities > 1 ) {
-				echo sprintf( '<button type="button" id="h5p-show-hide" class="btn btn-secondary btn-sm" show-all-text="%s" hide-all-text="%s" aria-label="%s"></button>', __( 'Expand all', 'pressbooks-book' ), __( 'Hide all', 'pressbooks-book' ), __( 'Expand all activities', 'pressbooks-book' ) );
+			foreach ( $data['activities'] as $activity ) {
+				$short_code = sprintf( '[h5p id="%s"]', $activity['ID'] );
+				echo '<tr>';
+				echo sprintf( '<td>%s</td>', $activity['ID'] );
+				echo sprintf( '<td>%s</td>', $activity['title'] );
+				echo sprintf( '<td>%s</td>', $activity['activity_type'] );
+				if ( ! isset( $_GET['h5p_id'] ) ) {
+					echo sprintf( '<td><button h5p-id="%s" type="button" show-activity-text="%s" hide-activity-text="%s" aria-label="%s" class="btn btn-secondary btn-sm h5p-row-item"></button></td>', $activity['ID'], __( 'Show activity', 'pressbooks-book' ), __( 'Hide activity', 'pressbooks-book' ), __( 'View h5p activity', 'pressbooks-book' ) );
+				}
+				echo '</tr>';
+				echo isset( $_GET['h5p_id'] ) ?
+					sprintf( '<tr><td colspan="4">%s</td></tr>', do_shortcode( $short_code ) ) :
+					sprintf( '<tr class="h5p-activity-container"><td colspan="4">%s</td></tr>', do_shortcode( $short_code ) );
 			}
 			?>
-			</p>
-			<table class="table table-borderless" id="h5p-listing-table">
-				<thead>
-				<tr>
-					<th><?php echo __( 'ID', 'pressbooks-book' ); ?></th>
-					<th><?php echo __( 'Title', 'pressbooks-book' ); ?></th>
-					<th><?php echo __( 'Activity type', 'pressbooks-book' ); ?></th>
-					<th><?php echo __( 'Show/Hide', 'pressbooks-book' ); ?></th>
-				</tr>
-				</thead>
-				<tbody>
-				<?php
-				foreach ( $data['activities'] as $activity ) {
-					$short_code = sprintf( '[h5p id="%s"]', $activity['ID'] );
-					echo '<tr>';
-					echo sprintf( '<td>%s</td>', $activity['ID'] );
-					echo sprintf( '<td>%s</td>', $activity['title'] );
-					echo sprintf( '<td>%s</td>', $activity['activity_type'] );
-					echo $activities === 1 ?
-						'<td></td>' :
-						sprintf( '<td><button h5p-id="%s" type="button" show-activity-text="%s" hide-activity-text="%s" aria-label="%s" class="btn btn-secondary btn-sm h5p-row-item"></button></td>', $activity['ID'], __( 'Show activity', 'pressbooks-book' ), __( 'Hide activity', 'pressbooks-book' ), __( 'View h5p activity', 'pressbooks-book' ) );
-					echo '</tr>';
-					echo $activities === 1 ?
-						sprintf( '<tr><td colspan="4">%s</td></tr>', do_shortcode( $short_code ) ) :
-						sprintf( '<tr class="h5p-activity-container"><td colspan="4">%s</td></tr>', do_shortcode( $short_code ) );
-				}
-				?>
-				</tbody>
-			</table>
+			</tbody>
+		</table>
 		<div class="h5p-pagination">
 			<?php echo $data['pagination']; ?>
 		</div>
