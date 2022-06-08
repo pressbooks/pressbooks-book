1f5336ff7075b5d4e00b071fd3d94b7779dd2066
SteelWagstaff <steel@pressbooks.com>
2022-06-08 12:56:39 -0700

    Update target URL

--- header.php
+++ header.php
@@ -100,7 +100,7 @@
 			</div>
 		</div>
 		<div class="cta">
-			<p><?php echo sprintf( esc_html__( 'Want to adapt books like this? %s about how Pressbooks supports open practices.', 'pressbooks-book' ), sprintf( '<a href="%1$s" target="_blank">%2$s</a>', esc_url( ' https://pressbooks.com' ), esc_html__( 'Learn more', 'pressbook-book' ) ) ); ?></p>
+			<p><?php echo sprintf( esc_html__( 'Want to adapt books like this? %s about how Pressbooks supports open practices.', 'pressbooks-book' ), sprintf( '<a href="%1$s" target="_blank">%2$s</a>', esc_url( ' https://pressbooks.com/adapt-open-books-with-Pressbooks' ), esc_html__( 'Learn more', 'pressbook-book' ) ) ); ?></p>
 		</div>
 		<?php if ( ! is_front_page() && pb_get_first_post_id() ) { ?>
 			<div class="reading-header">
