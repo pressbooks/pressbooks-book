1048242b4ec0da2cf22981243a8213210819b5ee
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-07-25 11:26:22 -0400

    chore: install rector

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $config): void {
+	$config->paths([
+		__DIR__ . '/bin',
+		__DIR__ . '/inc',
+		__DIR__ . '/partials',
+		__DIR__ . '/tests',
+	]);
+
+	// Directories to skip, rules I don't agree with...
+	$config->skip([
+		\Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class,
+		\Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class,
+	]);
+
+	// define sets of rules
+	$config->sets([
+		LevelSetList::UP_TO_PHP_80,
+	]);
+};
+
