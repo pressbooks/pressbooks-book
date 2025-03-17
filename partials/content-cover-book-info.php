ba96beeaccb6c07a5b764ef66e6c57fe4471cf28
Ricardo Aragon <ricardo@pressbooks.com>
2025-03-17 18:40:54 -0300

    fix: info and metadata buttons (#1313)
    Co-authored-by: Oscar Arzola <oscararzola@gmail.com>

--- partials/content-cover-book-info.php
+++ partials/content-cover-book-info.php
@@ -84,6 +84,15 @@
 	</div>
 
 	<div class="block-toggle__cta">
-		<a class="block-toggle__cta__button button--circle--primary js-toggle-block"><svg><use href="#arrow-down" /></svg><span class="screen-reader-text"><?php _e( 'Click for more information', 'pressbooks-book' ) ?></span></a>
+		<button
+			class="block-toggle__cta__button button--circle--primary js-toggle-block"
+			aria-expanded="false"
+			aria-controls="block-info"
+		>
+			<svg><use href="#arrow-down" /></svg>
+			<span class="screen-reader-text">
+				<?php _e( 'Click for more information', 'pressbooks-book' ) ?>
+			</span>
+		</button>
 	</div>
 </section>
