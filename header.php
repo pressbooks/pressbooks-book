b6502b839b7982467a39191020c3ed0c7f192bd9
Steel Wagstaff <steel@pressbooks.com>
2025-06-07 08:57:49 -0700

    fix: use site icon set in Aldine on books

--- header.php
+++ header.php
@@ -4,12 +4,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-16x16.png">
+	<?php
+	$root_id = get_network()->site_id;
+	switch_to_blog( $root_id );
+	wp_site_icon();
+	restore_current_blog();
+	?>
 	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
-	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/safari-pinned-tab.svg" color="#b01109">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.ico">
 	<meta name="application-name" content="Pressbooks">
 	<meta name="msapplication-TileColor" content="#b01109">
 	<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/browserconfig.xml">
@@ -65,7 +66,6 @@
 			<div class="header__inside">
 				<div class="header__brand">
 					<?php
-					$root_id = get_network()->site_id;
 					switch_to_blog( $root_id );
 					?>
 					<a aria-label="<?php echo get_bloginfo( 'name', 'display' ); ?>" href="<?php echo network_home_url(); ?>">
