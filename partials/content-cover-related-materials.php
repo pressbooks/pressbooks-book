80f1788d8c4f64ce09afbed9d135ad022084f630
Steel Wagstaff <steel@pressbooks.com>
2025-12-09 10:22:32 -0800

    fix: update styles

--- partials/content-cover-related-materials.php
+++ partials/content-cover-related-materials.php
@@ -10,28 +10,28 @@ if ( empty( $related_materials ) ) {
 }
 ?>
 
-<section class="block block-related-materials block-toggle js-block">
-	<h2 class="block__title block-related-materials__title" id="block-related-materials-title"><?php _e( 'Supplemental Materials', 'pressbooks-book' ); ?></h2>
-	<div class="block-related-materials__inner block-toggle__content" id="block-related-materials">
-		<div class="block-related-materials__inner__content">
-			<ul class="block-related-materials__list">
+<section class="block block-meta block-toggle js-block">
+	<div class="block-meta__content-box">
+		<h2 class="block__title block-meta__title" id="block-supplemental-materials-title"><?php _e( 'Supplemental Materials', 'pressbooks-book' ); ?></h2>
+
+		<div class="block-meta__inner block-toggle__content" id="block-supplemental-materials">
+			<dl>
 				<?php foreach ( $related_materials as $material ) : ?>
-					<li class="block-related-materials__item">
-						<a href="<?php echo esc_url( $material['url'] ); ?>" target="_blank" rel="noopener noreferrer">
-							<svg class="icon--svg" role="none" aria-hidden="true" focusable="false">
-								<use href="#icon-link" />
-							</svg>
-							<?php echo esc_html( ! empty( $material['title'] ) ? $material['title'] : $material['url'] ); ?>
-						</a>
+					<div class="block-meta__subsection">
+						<dt class="block__subtitle block-meta__subtitle">
+							<a href="<?php echo esc_url( $material['url'] ); ?>" target="_blank" rel="noopener noreferrer">
+								<?php echo esc_html( ! empty( $material['title'] ) ? $material['title'] : $material['url'] ); ?>
+							</a>
+							<?php if ( $material['privacy'] === 'private' && is_user_logged_in() && current_user_can( 'edit_posts' ) ) : ?>
+								<span class="private-badge"><?php _e( '(Private)', 'pressbooks-book' ); ?></span>
+							<?php endif; ?>
+						</dt>
 						<?php if ( ! empty( $material['description'] ) ) : ?>
-							<span class="block-related-materials__description"><?php echo esc_html( $material['description'] ); ?></span>
+							<dd class="ml0"><?php echo esc_html( $material['description'] ); ?></dd>
 						<?php endif; ?>
-						<?php if ( $material['privacy'] === 'private' && is_user_logged_in() && current_user_can( 'edit_posts' ) ) : ?>
-							<span class="block-related-materials__badge block-related-materials__badge--private"><?php _e( '(Private)', 'pressbooks-book' ); ?></span>
-						<?php endif; ?>
-					</li>
+					</div>
 				<?php endforeach; ?>
-			</ul>
+			</dl>
 		</div>
 		<?php
 		/**
@@ -42,13 +42,24 @@ if ( empty( $related_materials ) ) {
 		do_action( 'pb_book_cover_after_supplemental_materials' );
 		?>
 	</div>
+	<div class="block-toggle__cta">
+		<button
+			class="block-toggle__cta__button button--circle--primary js-toggle-block"
+			aria-expanded="false"
+			aria-controls="block-supplemental-materials"
+			aria-labelledby="block-supplemental-materials-title"
+		>
+			<svg><use href="#arrow-down" /></svg>
+		</button>
+	</div>
+</section>
 
 	<div class="block-toggle__cta">
 		<button
 			class="block-toggle__cta__button button--circle--primary js-toggle-block"
 			aria-expanded="false"
-			aria-controls="block-related-materials"
-			aria-labelledby="block-related-materials-title"
+			aria-controls="block-supplemental-materials"
+			aria-labelledby="block-supplemental-materials-title"
 		>
 			<svg><use href="#arrow-down" /></svg>
 		</button>
